40d0a240f506fa7179ec31563a073cf13fdea55b
krishpranav <krisna.pranav@gmail.com>
2021-06-19 09:23:38 +0530

    [+] tcpshark type struct [+]

--- tcpshark.go
+++ tcpshark.go
@@ -1,6 +1,27 @@
 package main
 
+import (
+	"log"
+
+	"github.com/google/gopacket"
+	"github.com/sachaos/tview"
+)
+
 const (
 	TailMode = iota
 	SelectMode
 )
+
+type Tcpshark struct {
+	src        *gopacket.PacketSource
+	view       *tview.Application
+	primitives []tview.Primitive
+	table      *tview.Table
+	detail     *tview.TextView
+	dump       *tview.TextView
+	frame      *tview.Frame
+	packets    []gopacket.Packet
+	mode       int
+
+	logger *log.Logger
+}
